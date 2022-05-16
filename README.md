59c27bff913f34b6237d5e6eca34a5e9a402bc86
locky <miaojihj@outlook.com>
2022-05-16 08:34:21 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+<!--
+ * @Description: 说明文档
+ * @LastEditors: hj
+ * @LastEditTime: 2022-05-16 08:17:24
+-->
 # hj-validate-demo
 It's a demo project, create by vue2 + element-ui + hj-validate
+
+## install
+```
+npm install
+```
+
+## dev
+```
+npm run serve
+```
+
+## build
+```
+npm run build
+```
+
---
+++ src/main.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @LastEditors: lianghui
+ * @LastEditTime: 2022-05-16 08:16:10
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
